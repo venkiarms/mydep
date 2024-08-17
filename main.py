0f92bb9f4c1a0a95e92b02ff708ddd36972cfb1b
venkiarms <108980881+venkiarms@users.noreply.github.com>
2024-08-17 15:43:07 +0530

    UpdateNew main.py

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
 import streamlit as st
+import streamlit_option_menu as option_menu
 
 
-st.header('Welcome to Vinsup Infotech AA')
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", "About"], icons=["house", "book"], menu_icon="cast", default_index=0)
 
-st.subheader('1. Tokens in C')
+    if selected == "Task-1 & 2":
+        st.subheader("Positive or Negative number:")
+        st.code("""
+
+           #include <stdio.h>
+int main()
+{
+    int num = 23;
+      
+    //Conditions to check if the number is negative/positive or zero
+    if (num > 0)
+         printf("The number is positive");
+    else if (num < 0)
+        printf("The number is negative");
+    else
+        printf("Zero");
+    
+    return 0;
+}
+
+        """, language="c")
 
 
+        st.subheader("Odd or Even number:")
 
-st.code("""
- # include
- int main()
- {
-      printf("my name is somya");
-      printf("%d",4);
- }""", language="c")
-# pip freeze requirements.txt
+        st.code("""
+           #include <stdio.h>
+int main()
+{
+    int num = 23;
+      
+    //Conditions to check if the number is odd/even
+    if (num % 2 == 0)
+         printf("The number is even");
+    else
+        printf("The number is odd");
+    
+    return 0;
+}
+                """,language="c")
+
+ 
+
