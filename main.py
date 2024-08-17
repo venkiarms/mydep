10fabc5910893494c7fad0045a302fca1fd2e0de
venkiarms <108980881+venkiarms@users.noreply.github.com>
2024-08-17 15:21:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ import streamlit as st
 
 st.header('Welcome to Vinsup Infotech AA')
 
+st.subheader('1. Tokens in C')
+
+
+
+st.code("""
+ # include
+ int main()
+ {
+      printf("my name is somya");
+      printf("%d",4);
+ }""", language="c")
 # pip freeze requirements.txt
